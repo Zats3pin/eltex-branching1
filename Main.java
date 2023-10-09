66cd3f0b31880fa9506f1b07f67e8a60719bf201
Zats3pin <zck2002@mail.ru>
2023-10-09 16:46:16 +0300

    пункт 31

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final var scanner = new Scanner(System.in);
-        System.out.print("Представтесь:");
-        final var name = scanner.next();
+        System.out.print("Представтесь: ");
+        final var name = scanner.nextLine();
         System.out.printf("Привет, %s%n", name);
     }
 }
